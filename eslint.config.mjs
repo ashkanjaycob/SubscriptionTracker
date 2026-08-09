39faa233dbb38a1fff9ec610f90c4d1f065488e3
ashkanjaycob <ashkan.yaghobii@gmail.com>
2026-08-09 13:31:24 +0400

    chore: add dist and node_modules to ESLint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**'],
+  },
   ...tseslint.configs.recommended,
   {
     rules: {
